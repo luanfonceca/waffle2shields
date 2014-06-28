9675b863c10cf1f337a42b68d17e31e026f08c40
Luan fonseca de farias <luanfonceca@gmail.com>
2014-06-28 10:00:29 -0300

    Create the Waffle2Shields app

---
+++ README.md
@@ -0,0 +1,9 @@
+Waffle.io to Shields.io
+============
+
+##### Use waffle.io badges like the shields.io badges
+
+Usage
+```
+http://waffle2shields.herokuapp.com/?user=luanfonceca&repo=speakerfight&label=ready&style=flat
+```
---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, redirect, request
+from urllib2 import urlopen
+from re import findall
+
+app = Flask(__name__)
+
+WAFFLE_URL = 'https://badge.waffle.io/{user}/{repo}.png?label={label}'
+SHIELDS_URL = ('http://img.shields.io/badge/'
+               '{subject}-{status}-{color}.svg?style={style}')
+
+
+@app.route("/")
+def parse_badges():
+    data = request.args.to_dict()
+    html = urlopen(WAFFLE_URL.format(**data)).read()
+    shields_data = {
+        'subject': data.get('label', 'Waffle').title(),
+        'status': findall(r'<text.*?>(.*?)</text>', html)[-1],
+        'color': findall(r'<rect.*? fill="#?([^\s^"]+)', html)[2],
+        'style': data.get('style')
+    }
+    return redirect(SHIELDS_URL.format(**shields_data))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
