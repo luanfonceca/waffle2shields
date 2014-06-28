7acb4eb8034c17be068040140ccb7232bb03c28e
Luan fonseca de farias <luanfonceca@gmail.com>
2014-06-28 10:33:58 -0300

    Return to the github page when there is no parameters.

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from re import findall
 
 app = Flask(__name__)
 
+GITHUB_URL = ('https://github.com/luanfonceca/'
+              'waffle2shields#waffleio-to-shieldsio')
 WAFFLE_URL = 'https://badge.waffle.io/{user}/{repo}.png?label={label}'
 SHIELDS_URL = ('http://img.shields.io/badge/'
                '{subject}-{status}-{color}.svg?style={style}')
@@ -12,6 +14,10 @@ SHIELDS_URL = ('http://img.shields.io/badge/'
 @app.route("/")
 def parse_badges():
     data = request.args.to_dict()
+
+    if not data:
+        return redirect(GITHUB_URL)
+
     html = urlopen(WAFFLE_URL.format(**data)).read()
     shields_data = {
         'subject': data.get('label', 'Waffle').title(),
